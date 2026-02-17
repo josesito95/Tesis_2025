5b19f19289de3f6558e79eefdb3a578cd39ec638
josesito95 <joseluisfarias117@gmail.com>
2026-02-17 18:44:50 -0300

    preparando transicion de nivel1 a nivel2
    preparando transicion de nivel1 a nivel2

--- Tesis_ola_2025/Assets/Scripts/Vision Cuadros/RevealCounter.cs
+++ Tesis_ola_2025/Assets/Scripts/Vision Cuadros/RevealCounter.cs
@@ -6,6 +6,9 @@ public class RevealCounter : MonoBehaviour
     public int total = 3;                 // ¿cuántos cuadros son?
     public PlayableDirector director;     // tu VisionDirector (el que tiene la Timeline)
 
+    [Header("Letra O final")]
+    public GameObject portalO;            // ACA vas a arrastrar "o de ola para gameplay"
+
     private int current = 0;              // cuenta actual
     private bool played = false;          // para no reproducir dos veces
 
@@ -14,10 +17,20 @@ public class RevealCounter : MonoBehaviour
     {
         if (played) return;
         current++;
+
         if (current >= total)
         {
             played = true;
             if (director != null) director.Play(); // ¡reproducir la Timeline!
+        }
+    }
+
+    // ESTE método lo va a llamar la Timeline con el Signal
+    public void ActivateO()
+    {
+        if (portalO != null)
+        {
+            portalO.SetActive(true);
         }
     }
 }
